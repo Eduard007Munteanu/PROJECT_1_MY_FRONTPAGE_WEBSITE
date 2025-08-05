33a3854a767eb8bc14994e82f0f825a95389b6a0
Eduard <au705518@uni.au.dk>
2025-08-05 11:23:10 +0200

    not quite working, needs to be fixed.

---
+++ backEnd/src/main/java/com/eduard/Application.java
@@ -0,0 +1,11 @@
+package com.eduard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
